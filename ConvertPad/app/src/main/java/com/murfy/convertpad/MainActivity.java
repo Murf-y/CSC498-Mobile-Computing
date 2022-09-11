ba1082eb1a1d6c179a17c79208b4fe2d81dc6bcb
Murf-y <charbelfayad64@gmail.com>
2022-09-11 11:27:34 +0300

    init project + add colors and vector assets :tada:

--- ConvertPad/app/src/main/java/com/murfy/convertpad/MainActivity.java
+++ ConvertPad/app/src/main/java/com/murfy/convertpad/MainActivity.java
@@ -1,11 +1,8 @@
-package com.murfy.calculator;
+package com.murfy.convertpad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    public void test(View v){
-        Toast.makeText(this, "hi",Toast.LENGTH_LONG).show();
     }
 }
