4fba1a772e3c0fb0139934759dfbf6cd95566e8d
Murf-y <charbelfayad64@gmail.com>
2022-09-27 22:30:28 +0300

    add home layout

--- Mews/app/src/main/java/com/murfy/mews/MainActivity.java
+++ Mews/app/src/main/java/com/murfy/mews/MainActivity.java
@@ -2,9 +2,12 @@ package com.murfy.mews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.murfy.mews.Models.User;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         current_user = (User) getIntent().getSerializableExtra("user");
 
         Log.i("DEBUG: ", current_user.getUserName());
