22afc508e0cc3fbb16060978c94fc080fb568a05
Murf-y <charbelfayad64@gmail.com>
2022-09-27 22:58:16 +0300

    finish dynamic content :rocket:

--- Mews/app/src/main/java/com/murfy/mews/Utils/DateHelper.java
+++ Mews/app/src/main/java/com/murfy/mews/Utils/DateHelper.java
@@ -8,15 +8,6 @@ import java.util.Date;
 
 public class DateHelper {
     public static String getTodayDateFormatted(){
-        try{
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", java.util.Locale.ENGLISH);
-            Date date= sdf.parse(String.valueOf(Calendar.getInstance().getTime()));
-            sdf.applyPattern("EEEE, MMM d");
-            String str = sdf.format(date);
-            return str;
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
+        return new SimpleDateFormat("EEEE, MMM d").format(new Date());
     }
 }
